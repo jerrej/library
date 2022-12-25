72023e776c422214d66e54f5d7e79aab6500322b
jerrej <20794967+jerrej@users.noreply.github.com>
2022-12-25 03:27:19 +0100

    Add more book attributes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 // Constructors
 
 class Book {
-    constructor(title, author) {
+    constructor(title, author, pages, isRead) {
       this.title = title;
       this.author = author;
+      this.pages = pages;
+      this.isRead = isRead;
     }
   }
 
@@ -27,10 +29,10 @@ console.log(booksList)
 
 // Dummy Data
 
-library.addBook(new Book("Brave New World","Aldous Huxley"))
-library.addBook(new Book("Nineteen Eighy-Four","George Orwell"))
-library.addBook(new Book("Mort","Terry Pratchett"))
-library.addBook(new Book("Fire & Blood","George R.R. Martin"))
+library.addBook(new Book("Brave New World","Aldous Huxley",50,false))
+library.addBook(new Book("Nineteen Eighy-Four","George Orwell",60,false))
+library.addBook(new Book("Mort","Terry Pratchett",70,false))
+library.addBook(new Book("Fire & Blood","George R.R. Martin",80,false))
 
 // Functions
 
@@ -39,22 +41,38 @@ function createTableRow(book) {
     const tableRow = document.createElement('tr')
     const rowTitle = document.createElement('td')
     const rowAuthor = document.createElement('td')
+    const rowPages = document.createElement('td')
+    const rowIsRead = document.createElement('td')
 
     rowTitle.textContent = book.title
     rowAuthor.textContent = book.author
+    rowPages.textContent = book.pages
+    rowIsRead.textContent = book.isRead
 
     tableRow.appendChild(rowTitle)
     tableRow.appendChild(rowAuthor)
+    tableRow.appendChild(rowPages)
+    tableRow.appendChild(rowIsRead)
     booksTable.appendChild(tableRow)
 
 }
 
-// Load page
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+
+// Load Page
 
 for (let book of library.books) {
     console.log(book.title)
     createTableRow(book)
 }
+
+closeForm()
 
 
 console.log("End of javascript reached")
